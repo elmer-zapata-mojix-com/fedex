ac08a1759f58e801bd0863d98973a023fa6b7f10
eynar-pari <eynar_repc@hotmail.com>
2023-05-10 19:30:40 -0400

    added  basic structure

---
+++ SeleniumTraining/src/code/page/LoginSection.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using SeleniumTraining.src.code.control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTraining.src.code.page
+{
+    public class LoginSection
+    {
+        public TextBox emailTxtBox = new TextBox(By.Id("ctl00_MainContent_LoginControl1_TextBoxEmail"));
+        public TextBox pwdTxtBox = new TextBox(By.Id("ctl00_MainContent_LoginControl1_TextBoxPassword"));
+        public Button loginButton = new Button(By.Id("ctl00_MainContent_LoginControl1_ButtonLogin"));
+    }
+}
